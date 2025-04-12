7d91972cf24821b2bedb03621519c6b57d2b5831
Joshua Anderson <andersmmg@gmail.com>
2025-04-12 17:02:49 -0600

    fix: timeout when loading data

--- project/singletons/http.gd
+++ project/singletons/http.gd
@@ -3,6 +3,7 @@ extends Node
 func async_request(url: String, custom_headers := PackedStringArray(), method := HTTPClient.Method.METHOD_GET, request_data := "") -> HTTPResult:
 	var new_requester := HTTPRequest.new()
 	add_child(new_requester)
+	new_requester.timeout = 5.0
 	var err := new_requester.request(url, custom_headers, method, request_data)
 	if err:
 		new_requester.queue_free()
